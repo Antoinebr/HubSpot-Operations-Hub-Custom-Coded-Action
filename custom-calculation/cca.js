ebc8755dea9ee28f3b0b58502187a5067a983657
Antoine Brossault <antoinebrossault@gmail.com>
2022-12-12 13:53:27 +0100

    custom-calculation

---
+++ custom-calculation/cca.js
@@ -0,0 +1,47 @@
+exports.main = async (event, callback) => {
+
+    const daysRemaining = () => {
+        // Create a Date object for the current date
+        const currentDate = new Date();
+        
+        // Create a Date object for the end of the current year
+        // by specifying the year, month (0-11), and day (1-31)
+        const endOfYear = new Date(currentDate.getFullYear(), 11, 31);
+        
+        // Calculate the difference in milliseconds between the two dates
+        const diff = endOfYear - currentDate;
+        
+        // Calculate the number of days by dividing the difference in milliseconds by
+        // the number of milliseconds in a day (1000 * 60 * 60 * 24)
+        const daysRemaining = Math.ceil(diff / (1000 * 60 * 60 * 24));
+        
+        // Return the number of days remaining
+        return daysRemaining;
+      }
+
+
+    if (!event.inputFields.dealAmount) throw new Error('dealAmount has to be setup in the "Property to include in code" section ');
+    if (!event.inputFields.probability) throw new Error('probability has to be setup in the "Property to include in code" section ');
+    if (!event.inputFields.landAndExpand) throw new Error('landAndExpand has to be setup in the "Property to include in code" section ');
+
+
+    const dealAmount = event.inputFields.dealAmount;
+
+    const probability = event.inputFields.probability;
+
+    const landAndExpand = (event.inputFields.landAndExpand === "true") ? 1.25 : 1;
+
+    /**
+        Deal amount * Probability * Days left in the year * Land and expand (1.25 for yes, 1.00 for no)
+    */
+
+    const customForecastAmount = (probability * daysRemaining() * landAndExpand)
+
+
+    callback({
+        outputFields: {
+            customForecastAmount
+        }
+    });
+
+}
---
+++ custom-calculation/event.js
@@ -0,0 +1,10 @@
+exports.events = {
+    // object: {
+    //     objectId: 3401
+    // },
+    inputFields: {
+        landAndExpand : "true",
+        dealAmount : "56000",
+        probability: "0.20"
+    }
+}
